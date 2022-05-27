dd8782495fe0d2351b5ef62fb34e770c7ce1cc0b
Maxime Gaillard <maxgaillard@gmail.com>
2022-05-27 19:59:13 -0400

    Creating, Reading and Writing files

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,14 +1,40 @@
+// const fs = require('fs');
+
+// const book = {
+//   title: 'Ego is the enemy',
+//   author: 'Ryan Holiday'
+// };
+
+// const bookJSON = JSON.stringify(book);
+// fs.writeFileSync('1-json.json', bookJSON);
+
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title);
+// const fs = require('fs');
+
 const fs = require('fs');
 
-const book = {
-  title: 'Ego is the enemy',
-  author: 'Ryan Holiday'
+const humanoid = {
+  name: 'Little green man',
+  planet: 'Mars',
+  age: 2000
 };
 
-const bookJSON = JSON.stringify(book);
+const humanoidJSON = JSON.stringify(humanoid);
+fs.writeFileSync('2-json.json', humanoidJSON);
 
-fs.writeFileSync('1-json.json', bookJSON);
-const dataBuffer = fs.readFileSync('1-json.json');
-const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data.title);
+const dataBuffer = fs.readFileSync('2-json.json');
+const data = JSON.parse(dataBuffer);
+
+// console.log(data);
+
+data.name = 'Buck Rogers';
+data.planet = 'Earth';
+data.age = 40;
+
+const mutateHumanoid = JSON.stringify(data);
+fs.writeFileSync('2-json.json', mutateHumanoid);
+
+console.log(fs.readFileSync('2-json.json').toString());
